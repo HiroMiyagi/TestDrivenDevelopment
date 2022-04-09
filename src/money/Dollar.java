972a39598fb1110af4a788849dbc2f7b45400ca5
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-04-09 21:19:37 +0900

    First commit

---
+++ src/money/Dollar.java
@@ -0,0 +1,9 @@
+package money;
+
+public class Dollar {
+	int amount = 10;
+	Dollar(int amount) {		
+	}
+	void times(int multiplier) {
+	}
+}
---
+++ src/money/MoneyTest.java
@@ -0,0 +1,15 @@
+package money;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+	@Test
+	public void testMultiplication() {
+	Dollar five = new Dollar(5);
+	five.times(2);
+	assertEquals(10, five.amount);
+	}
+	
+}
