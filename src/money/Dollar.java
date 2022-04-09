2ea7e104e01f7bc99bc9121164a352e8e3b92ca0
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-04-09 21:46:45 +0900

    amountとmultiplierを定義

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -2,9 +2,10 @@ package money;
 
 public class Dollar {
 	int amount;
-	Dollar(int amount) {		
+	Dollar(int amount) {
+		this.amount = amount;
 	}
 	void times(int multiplier) {
-		amount = 5 * 2;
+		amount *= multiplier;
 	}
 }
