08186ed5bc0e855a342e06cbd718ff463964ed7c
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-04-13 12:45:18 +0900

    Add Franc Test & Franc class

---
+++ src/money/Franc.java
@@ -0,0 +1,15 @@
+package money;
+
+public class Franc {
+	private int amount;
+	Franc(int amount) {
+		this.amount = amount;
+	}
+	Franc times(int multiplier) {
+		return new Franc(amount * multiplier);
+	}
+	public boolean equals(Object object) {
+		Franc franc = (Franc) object;
+		return amount == franc.amount;
+	}
+}
