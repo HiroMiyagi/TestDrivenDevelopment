ff183f4d00b20e7b3329f39d3acab64c8cffe310
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-04-15 13:07:31 +0900

    Add Franc test & Move equals

--- src/money/Franc.java
+++ src/money/Franc.java
@@ -1,15 +1,10 @@
 package money;
 
-public class Franc {
-	private int amount;
+class Franc extends Money {
 	Franc(int amount) {
 		this.amount = amount;
 	}
 	Franc times(int multiplier) {
 		return new Franc(amount * multiplier);
-	}
-	public boolean equals(Object object) {
-		Franc franc = (Franc) object;
-		return amount == franc.amount;
 	}
 }
