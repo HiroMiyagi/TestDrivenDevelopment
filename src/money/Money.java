56581283a8baebea48389d95d35d3f4f45828122
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-04-18 11:26:48 +0900

    Changed to return money type from dollar & franc type

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,10 +1,17 @@
 package money;
 
-class Money {
+abstract class Money {
 	protected int amount;
+	abstract Money times(int multipier);
 	public boolean equals(Object object) {
 		Money money = (Money) object;
 		return amount == money.amount
 				&& getClass().equals(money.getClass());
+	}
+	static Money dollar(int amount) {
+		return new Dollar(amount);
+	}
+	static Money franc(int amount) {
+		return new Franc(amount);
 	}
 }
