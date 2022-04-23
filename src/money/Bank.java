c93b4344fd8ceb47f900d9afbe092e2cdcbb270d
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-04-23 13:16:08 +0900

    Add rate

--- src/money/Bank.java
+++ src/money/Bank.java
@@ -2,6 +2,12 @@ package money;
 
 class Bank {
 	Money reduce(Expression source, String to) {
-		return source.reduce(to);
+		return source.reduce(this, to);
+	}
+	void addRate(String from, String to, int rate) {
+		
+	}
+	int rate(String from, String to) {
+		return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
 	}
 }
