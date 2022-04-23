aa2a60f39da2b8d3e771dd6ca19d140a3b60c668
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-04-23 09:06:53 +0900

    Add plus method & Bank class

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-class Money {
+class Money implements Expression {
 	protected int amount;
 	protected String currency;
 	Money(int amount, String currency) {
@@ -9,6 +9,9 @@ class Money {
 	}
 	Money times(int multiplier) {
 		return new Money(amount * multiplier, currency);
+	}
+	Money plus(Money addend) {
+		return new Money(amount + addend.amount, currency);
 	}
 	String currency() {
 		return currency;
