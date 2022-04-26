edb43396158e663ebd5ad03443caf2f5789f8419
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-04-26 10:38:17 +0900

    Add Sum.plus

--- src/money/Sum.java
+++ src/money/Sum.java
@@ -8,7 +8,7 @@ class Sum implements Expression {
 		this.addend = addend;		
 	}
 	public Expression plus(Expression addend) {
-		return null;
+		return new Sum(this, addend);
 	}
 	public Money reduce(Bank bank, String  to) {
 		int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
